fe3ba873d95276ae3083cce9ca077e2998974f8f
soany257 <soanyaparicio75@gmail.com>
2025-04-21 11:42:56 -0600

    creacion de mi parte

--- proyecto.py
+++ proyecto.py
@@ -1,1 +1,15 @@
 print("Hola a todos")
+
+
+def verificar_ganador(tablero, jugador):
+    # Verificar filas, columnas y diagonales
+    for fila in tablero:
+        if all(celda == jugador for celda in fila):
+            return True
+    for col in range(3):
+        if all(tablero[fila][col] == jugador for fila in range(3)):
+            return True
+    if all(tablero[i][i] == jugador for i in range(3)) or \
+       all(tablero[i][2 - i] == jugador for i in range(3)):
+        return True
+ 
